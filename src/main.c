479c0fc7c3d30516b293aaf6744e907addde593f
João Victor Batista <jvtexbat@hotmail.com>
2023-01-18 23:47:19 -0300

    Implement bme280 reader

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "bme280.h"
 
+int main()
+{
+    printf("FSE Trabalho 2\n");
+    float external_temp = 0;
 
-int main(){
-  printf("FSE Trabalho 2\n");
+    while (1)
+    {
+        getBMETemperature(&external_temp);
+        if (external_temp == -1.0) external_temp = 0;
 
-  return 0;
+        printf("Temp Ext: %.2f\n\n", external_temp);
+
+        sleep(1);
+    }
+
+    return 0;
 }
